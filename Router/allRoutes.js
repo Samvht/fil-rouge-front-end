b0cfa1c62123153f4824cf111e78e6ef7a42d8b1
Samvht <samvht@hotmail.com>
2025-04-17 11:14:47 +0200

    Ajout de la route galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/indexfront.html", "Accueil", "/pages/home.html"),
+    new Route("/indexfront.html/galerie", "Galerie", "/pages/galerie.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
