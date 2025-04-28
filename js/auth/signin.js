6d55cc8317186067835e8a3ffcb6f570e6d2eba2
Samvht <samvht@hotmail.com>
2025-04-28 13:50:44 +0200

    connexion + deconnexion

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -3,3 +3,24 @@ const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 //Récupération bouton de connexion
 const btnSingin = document.getElementById("btnSignin");
+
+btnSingin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    //Ici, il faudra appeler l'API pour vérification des données en BDD
+
+    //Test avec données factives
+    if(mailInput.value == "cerise@mail.com" && passwordInput.value == "cerise357"){
+        alert("connexion réussie");
+        //Il faudra récupérer le vrai token, const pour la valeur du token
+        const token = "aabeo";
+        //place ce token en cookie (Ici acesstoken)
+        setToken(token);
+        
+        window.location.replace("/indexfront.html/mesResa");
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+//const pour le nom du cookie
+const tokenCookieName = "accesstoken";
+const signoutBtn = document.getElementById("signout-btn");
+
+signoutBtn.addEventListener("click", signout);
+
+function signout(){
+    eraseCookie(tokenCookieName);
+    window.location.reload();
+}
+
+function setToken(token){
+    setCookie(tokenCookieName, token, 7);
+}
+
+function getToken(){
+    return getCookie(tokenCookieName);
+}
+
+//Fonction crée et stocke un cookie (nom cookie, valueur donné au cookie, nbre de jours avant expiration)
+function setCookie(name,value,days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+//Récupérer cookie par son nom
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(const element of ca) {
+        let c = element;
+        while (c.startsWith(' ')) c = c.substring(1,c.length);
+        if (c.startsWith(nameEQ)) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+//Effacer le cookie (une date passée pour être sur que le cookie soit bien supprimé)
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function isConnected(){
+    if(getToken() == null || getToken == undefined){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
